ee1ea4c58c6f0f6cee878b00c8da973ba7a354c6
KangsanBae <warimo47@gmail.com>
2022-05-20 18:00:42 +0900

    ArUco marker test QR code test

---
+++ OpenCVResearch/ArUcoMarkerTest.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) Kangsan Bae warimo47@naver.com All rights reserved.
+
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "ArUco/aruco.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+int main()
+{
+    cv::Mat image = cv::imread("test2.png");
+
+    aruco::MarkerDetector MDetector;
+
+    // aruco::Dictionary::DICT_TYPES::ARUCO_MIP_36h12;
+    // MDetector.setDictionary("ARUCO_MIP_36h12");
+
+    // detect
+    std::vector<aruco::Marker> markers = MDetector.detect(image);
+
+    // print info to console
+    for (size_t i = 0; i < markers.size(); ++i)
+    {
+        std::cout << markers[i] << std::endl;
+
+        // draw in the image
+        markers[i].draw(image);
+    }
+
+    cv::namedWindow("ArUco Marker Test", 1);
+    cv::imshow("ArUco Marker Test", image);
+    cv::waitKey(0);
+}
---
+++ OpenCVResearch/QRCodeTest.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) Kangsan Bae warimo47@naver.com All rights reserved.
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+int main()
+{
+    cv::Mat img = cv::imread("QR_Code_Images/qr_code_test_108px_532_372.jpg");
+
+    cv::QRCodeDetector decoder = cv::QRCodeDetector();
+    std::vector<cv::Point> points;
+    std::string data = decoder.detectAndDecode(img, points);
+
+    std::cout << "\nDecoded data: " << data << std::endl << std::endl;
+
+    cv::Point pt1;
+    cv::Point pt2;
+
+    for (int i = 0; i < points.size(); ++i)
+    {
+        pt1 = points[i];
+        pt2 = points[(i + 1) % 4];
+        line(img, pt1, pt2, cv::Scalar(255, 0, 0), 3);
+    }
+
+    cv::imshow("Detected QR code", img);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    return 0;
+}
